6cbc34012933b1a35898468c5b17c0dc143b3f4b
Cuttlerat <avkioller@gmail.com>
2017-08-21 13:03:17 +0300

    New weather

--- pybot.py
+++ pybot.py
@@ -24,9 +24,52 @@ def unknown(bot, update):
 
     bot.send_message(chat_id=update.message.chat_id, text="No such command")
 
+def get_emoji(emoji_code):
+
+    emoji_code = str(emoji_code)
+    print(emoji_code)
+
+    if "113" in emoji_code:
+        emoji_code = emoji_code.replace("113","☀️")
+
+    if "116" in emoji_code:
+        emoji_code = emoji_code.replace("116","🌥")
+
+    if "119" in emoji_code:
+        emoji_code = emoji_code.replace("119","🌥")
+
+    if "122" in emoji_code:
+        emoji_code = emoji_code.replace("122","☁️")
+
+    if "200" in emoji_code:
+        emoji_code = emoji_code.replace("200","🌩")
+
+    for i in ["143","248","260"]:
+        if i in emoji_code:
+            emoji_code = emoji_code.replace(i,"🌫")
+
+    for i in ["176","263","266","281","293","296","299","302","311","317","362"]:
+        if i in emoji_code:
+            emoji_code = emoji_code.replace(i,"🌧")
+
+    for i in ["179","182","185","227","230","323","326","329","332","335","338","350","353","368","371","374","377"]:
+        if i in emoji_code:
+            emoji_code = emoji_code.replace(i,"🌨")
+
+    for i in ["284","305","308","314","320","356","359","365"]:
+        if i in emoji_code:
+            emoji_code = emoji_code.replace(i,"🌧")
+
+    for i in ["386","389","392","395"]:
+        if i in emoji_code:
+            emoji_code = emoji_code.replace(i,"⛈")
+
+    return(emoji_code)
+
+print(get_emoji(113))
 def weather(bot, update, args):
 
-    city = ' '.join(args) if isargs else 'Ленинград'
+    city = ' '.join(args) if args else 'Ленинград'
 
     w_params = {        'q': city, 
                       'key': WEATHER_TOKEN, 
@@ -37,22 +80,53 @@ def weather(bot, update, args):
 
     w_response = requests.get('https://api.worldweatheronline.com/premium/v1/weather.ashx', w_params).json()
 
-    temp  = w_response["data"]["current_condition"][0]["temp_C"]
-    if temp[0] != '-': temp='+'+temp
+    now_temp  = w_response["data"]["current_condition"][0]["temp_C"]
+    if now_temp[0] != '-': now_temp='+'+now_temp
 
-    value = w_response["data"]["current_condition"][0]["lang_ru"][0]["value"]
-    city  = w_response["data"]["request"][0]["query"]
+    now_comment = w_response["data"]["current_condition"][0]["lang_ru"][0]["value"]
+    now_city  = w_response["data"]["request"][0]["query"]
 
-    time  = datetime.strptime(w_response["data"]["current_condition"][0]["observation_time"] + " 2017", '%I:%M %p %Y')
-    time  = pytz.timezone('Europe/Moscow').fromutc(time)
-    time  = "{:%H:%M}".format(time)
+    now_time  = datetime.strptime(w_response["data"]["current_condition"][0]["observation_time"] + " 2017", '%I:%M %p %Y')
+    now_time  = pytz.timezone('Europe/Moscow').fromutc(now_time)
+    now_time  = "{:%H:%M}".format(now_time)
+    now_emoji = get_emoji(w_response["data"]["current_condition"][0]["weatherCode"])
+
+    weather = {}
+    for j in range(2):
+        for i in range(3):
+            weather[j,"temp",i]    = w_response["data"]["weather"][j]["hourly"][2+(i*3)]["tempC"]
+            if weather[j,"temp",i][0] != '-': weather[j,"temp",i]='+'+weather[j,"temp",i]
+            weather[j,"emoji",i]   = get_emoji(w_response["data"]["weather"][j]["hourly"][2+(i*3)]["weatherCode"])
+            weather[j,"comment",i] = w_response["data"]["weather"][j]["hourly"][2+(i*3)]["lang_ru"][0]["value"]
     
-    message = ''.join("[{0}]: {1} {2}\n{3}".format(time,temp,value,city))
-    bot.send_message(chat_id=update.message.chat_id, text = message )
+    message = ''.join("""
+    *Now:* 
+    *[{0}]:* {1} {2} {3}
+    {4}
+
+    *Today:*
+    *Morning:* {5} {6} {7}
+    *Noon:* {8} {9} {10}
+    *Evening:* {11} {12} {13}
+
+    *Tommorow:* 
+    *Morning:* {14} {15} {16}
+    *Noon:* {17} {18} {19}
+    *Evening:* {20} {21} {22}
+    """.format(now_time,
+               now_emoji,
+               now_temp,
+               now_comment,
+               now_city,
+               *[ weather[i] for i in weather ] ))
+   
+    message = "\n".join([ k.strip() for k in message.split('\n') ])
+    
+    bot.send_message(chat_id=update.message.chat_id, parse_mode = "markdown", text = message )
 
     # LOG
     log_dict = {'timestamp': datetime.now().strftime("[%H:%M]"), 
-                  'message': message.replace('\n', ' '), 
+                  'message': "Weather {0}".format(now_city), 
                  'username': update.message.from_user.username }
     print("{timestamp}: \"{message}\" by @{username}".format(**log_dict))
 
