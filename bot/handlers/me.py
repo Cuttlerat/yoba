ea59a7e9814215ea76b95bff35a6818a0c131cff
Roman Zayrullin <zayrullin.roman@220-volt.ru>
2019-01-25 16:50:34 +0300

    Add little hug_formatter for markdown messages
    - Make sended text by `/me` __italic__.
    - Add **bold** analogue for future use.

--- bot/handlers/me.py
+++ bot/handlers/me.py
@@ -4,11 +4,14 @@ from sqlalchemy import and_
 from config import Config
 from logger import log_print
 from models.models import connector, Pingers
+from utils import italize
 import random
 
 
 
 def me(config, bot, update, *args, **kwargs):
+    import telegram
+
     username = update.message.from_user.username
     with connector(config.engine()) as ses:
         user_matches = ses.query(Pingers).filter(Pingers.chat_id == update.message.chat_id,
@@ -18,10 +21,12 @@ def me(config, bot, update, *args, **kwargs):
             match = random.choice([x.match for x in user_matches if x.me == 1])
         else:
             match = username
-        out_text="{match} {message}".format(match=match,
-                                            message=update.message.text.split(' ', 1)[1])
+        out_text=italize("{match} {message}".format(
+            match=match,
+            message=update.message.text.split(' ', 1)[1]))        
         bot.send_message(chat_id=update.message.chat_id,
-                         text=out_text)
+                         text=out_text,
+                         parse_mode=telegram.ParseMode.MARKDOWN)
         bot.delete_message(chat_id=update.message.chat_id,
                            message_id=update.message.message_id, *args, **kwargs)
         log_print('Me by {1}'.format(username, update.message.from_user.username))
