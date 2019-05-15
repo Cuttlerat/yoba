e60f6dfc2323016ee88d2dc71cb83fa4d6ea8c21
Cuttlerat <avkioller@gmail.com>
2019-05-15 19:18:48 +0300

    Hotfix

--- bot/handlers/spam_filter.py
+++ bot/handlers/spam_filter.py
@@ -1,7 +1,7 @@
 from logger import log_print
 from models.models import connector, Spam
 from sqlalchemy import and_
-from datetime import datetime
+from datetime import datetime, timedelta
 import redis
 
 def spam_check(config, bot, update, args):
@@ -37,7 +37,7 @@ def spam_set(config, bot, update, args):
     redis_db = config.redis
     new_req = args[0] if args[0] else 0
 
-    tomorrow = datetime.now() + datetime.timedelta(1)
+    tomorrow = datetime.now() + timedelta(1)
     midnight = datetime(year=tomorrow.year,
                         month=tomorrow.month,
                         day=tomorrow.day,
