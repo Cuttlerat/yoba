37dc142ce577dc61a71523bf4af75dd47997d1f0
Cuttlerat <avkioller@gmail.com>
2019-05-15 18:39:31 +0300

    Hotfix

--- bot/handlers/spam_filter.py
+++ bot/handlers/spam_filter.py
@@ -4,7 +4,7 @@ from sqlalchemy import and_
 import datetime
 import redis
 
-def spam_check(config, update, bot, args):
+def spam_check(config, bot, update, args):
 
     with connector(config.engine()) as ses:
         spamers = ses.query(Spam.username, Spam.requests).filter(Spam.chat_id == update.message.chat_id).distinct().all()
