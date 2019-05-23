61502a1fd480f63a26b880735e0bb13973a8cd36
Aleksei Kioller <avkioller@gmail.com>
2019-05-23 14:28:10 +0300

    Markdown doesn't escape in /me (#128)
    * Trying with HTML parse mode
    
    * Add escape for html

--- bot/utils.py
+++ bot/utils.py
@@ -2,8 +2,8 @@ from functools import partial
 
 
 def _hug_text(text, formatter):
-    return f'{formatter}{text}{formatter}'
+    return f'<{formatter}>{text}</{formatter}>'
 
 
-italize = partial(_hug_text, formatter='_')
-boldize = partial(_hug_text, formatter='*')
+italize = partial(_hug_text, formatter='i')
+boldize = partial(_hug_text, formatter='b')
