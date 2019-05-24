b910f375e669309ca1a8a881bd2b804bf5ff205c
Cuttlerat <avkioller@gmail.com>
2019-05-24 18:41:03 +0300

    Add emoji to wind

--- bot/handlers/weather.py
+++ bot/handlers/weather.py
@@ -199,7 +199,7 @@ def get_emoji(weather_status):
     return "".join([emojis[i] for i in emojis if weather_status == i])
 
 def degrees_to_cardinal(d):
-    dirs = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE",
-            "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
-    ix = int((d + 11.25)/22.5)
-    return dirs[ix % 16]
+    dirs = [u'\U0002B06', u'\U0002197', u'\U00027A1', u'\U0002198',
+            u'\U0002B07', u'\U0002199', u'\U0002B05', u'\U0002196']
+    ix = int((d + 22.5)/43)
+    return dirs[ix % 8]
